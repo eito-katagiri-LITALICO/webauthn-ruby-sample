33e4f887d8f663472bca79c762d7fe59eb586a36
Eito Katagiri <eito.katagiri@litalico.co.jp>
2023-11-24 00:56:44 +0900

    try to use tailwindcss on page and component

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 document.addEventListener("DOMContentLoaded", () => {
     const app = document.querySelector("#HomeIndex");
     if (app) {
-        ReactDOM.render(<div>Hello, React!</div>, app);
+        ReactDOM.render(<div className="font-bold underline">Hello, React!</div>, app);
     }
 });
